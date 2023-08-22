d8b6dbdce7f1a8342c88a6da2a01a3f5643def02
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-08-22 13:00:38 -0600

    Added a header.

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,1 +1,15 @@
-//hola
+/*
+------------------------------------
+Factorial.cpp
+------------------------------------
+UNIVERSIDAD DEL VALLE DE GUATEMALA
+CC3086 - Programación de Microprocesadores
+Laboratorio No.3
+------------------------------------
+
+Paralelizar código que calcula el 
+factorial de un número ingresado
+por el usuario
+
+------------------------------------
+*/
