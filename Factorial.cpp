8bc6d21c52ab49ce4cb8dd7f7f5003d836e480b2
Rodrimansidub14 <man22611@uvg.edu.gt>
2023-08-22 13:51:01 -0600

    Update Factorial 2

--- Factorial.cpp
+++ Factorial.cpp
@@ -16,23 +16,49 @@ por el usuario
 
 
 #include <iostream>
+#include <omp.h>
 using namespace std;
 // function to find factorial of given number
-unsigned int factorial(unsigned int n)
+unsigned long long factorial(unsigned int n)
 {
 if (n == 0)
 return 1;
-return n * factorial(n - 1);
+//define la funci[on de factorial usando programacion paralela. 
+unsigned long long factorial = 1;
+////Inicializamos la region paralela que distribuye las iteraciones en varios threads]
+#pragma omp parallel for reduction (*: factorial)
+for (unsigned int i = 1; i <= n; ++i){
+
+    /* #pragma omp critical permite que la seccion dentro del for sea ejecutada de manera segura sin 
+     ejecutar dos operaciones en simultaneo*/
+    #pragma omp critical
+    factorial *= i;
+}
+return factorial;
 }
 // Driver code
 int main()
 {
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+int num;
+// Pide el input al usuario
+
+cout << "Ingrese un numero"; 
+cin >> num; 
+=======
+>>>>>>> Stashed changes
 
 cout << "Ingrese el número para el cual desea calcular el factorial.";
 
 int num;
 cin >> num;
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> 2de4684926527d3fb153a58aa434694ccb1a2569
+>>>>>>> Stashed changes
 cout << "Factorial of "
 << num << " is " << factorial(num) << endl;
 return 0;
